dc5855c0709df566db515d52f1dbd557cf2c5d11
ParbinShreesh <parbinshreesh@ParbinShreeshs-MacBook-Air.local>
2026-03-03 18:44:24 +0545

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -19,5 +19,5 @@ export default defineConfig({
 
   // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
-  base: process.env.VITE_BASE_PATH || "/React-deploy"
+  base: "/",
 })
